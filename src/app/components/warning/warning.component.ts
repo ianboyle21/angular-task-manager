b10441ba1f5f136466c7c85a813ae2488851b16a
Ian Boyle <ian.boyle@alight.com>
2022-03-24 11:39:10 -0400

    adds username input

--- src/app/components/warning/warning.component.ts
+++ src/app/components/warning/warning.component.ts
@@ -7,11 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WarningComponent implements OnInit {
   warningSign: string = "ButtonText"
+  inputUserName = ""
+  userName: string = ""
+  emptyString = false;
+  toggleUserName = false;
   constructor() { }
   toggle: boolean = true;
   toggleWarning(){
     this.toggle = !this.toggle;
     this.warningSign = this.toggle ? 'Green Button Text' : 'Red Button Text';
+  }
+  addUserName() {
+    if (this.userName === ''){
+      this.toggleUserName = false;
+    }
+    this.toggleUserName = this.userName === '' ? false : true;
+    this.userName = this.toggleUserName ? this.inputUserName : ''
+    this.inputUserName = ''
+    console.log(this.userName, this.inputUserName, this.toggleUserName)
+  }
+
+  onEnterUserName(event: Event) {
+    this.userName = this.userName.length === 0 ? '' : (<HTMLInputElement>event.target).value
   }
   onEnterInputText(event: Event) {
     console.log((<HTMLInputElement>event.target).value)
