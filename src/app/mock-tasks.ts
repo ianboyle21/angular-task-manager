27b5f16b85b1e19fb1940ca25500dce3d78fa9d7
Ian Boyle <ian.boyle@alight.com>
2022-03-24 09:25:10 -0400

    initial commit. adds header, task and task-item component

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,21 @@
+import {Task} from './Task'
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Wash dishes',
+        day: 'March 23rd at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Fold Clothes',
+        day: 'March 23rd at 5:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Grocery',
+        day: 'March 27th at 10:30am',
+        reminder: false,
+    },
+]
